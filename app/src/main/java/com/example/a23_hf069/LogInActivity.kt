8f55443c5d82539b376af154aafa6007a32fb745
catapillar0505 <twrp3301@gmail.com>
2023-06-28 23:18:53 +0900

    databinding 오류 수정

--- app/src/main/java/com/example/a23_hf069/LogInActivity.kt
+++ app/src/main/java/com/example/a23_hf069/LogInActivity.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a23_hf069.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import androidx.viewpager2.widget.ViewPager2
 
 class LogInActivity : AppCompatActivity(){
     //
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val tabTextList = listOf("Profile", "Search")
+    private val tabTextList = listOf("개인회원 로그인", "기업회원 로그인")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         // Set up the adapter
-        binding.viewPager01.adapter = ViewPagerAdapter(this)
+        binding.viewpager01.adapter = ViewPagerAdapter(this)
 
-        TabLayoutMediator(binding.tabLayout01, binding.viewPager01) { tab, pos ->
+        TabLayoutMediator(binding.tablayout01, binding.viewpager01) { tab, pos ->
             tab.text = tabTextList[pos]
         }.attach()
     }
--- app/src/main/java/com/example/a23_hf069/P_login.kt
+++ app/src/main/java/com/example/a23_hf069/P_login.kt
@@ -1,48 +1,22 @@
 package com.example.a23_hf069
 
-/**
- * A simple [Fragment] subclass.
- * Use the [P_login.newInstance] factory method to
- * create an instance of this fragment.
- */
-class P_login : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.a23_hf069.databinding.FragmentPLoginBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+class P_login : Fragment() {
+    lateinit var binding: FragmentPLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_p_login, container, false)
-    }
+        binding = FragmentPLoginBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment p_login.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            P_login().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }
